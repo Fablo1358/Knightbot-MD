25aa267ca82b98166c177f18b4ce9a82f9a13e0a
Fablo1358 <133812127+Fablo1358@users.noreply.github.com>
2025-04-06 01:08:28 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Fabi bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '4915901907126', //Your number
+  botName: "Knight Bot",
+  botOwner: '∆∆∆∆', // Your name
+  ownerNumber: '15901907126', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
